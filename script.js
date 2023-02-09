9e3b58223ec0092ce2cd20ba237ae1b34819e2ce
moyinnn <moyinoluwaoso18@gmail.com>
2023-02-09 11:23:28 +0100

    pointers

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-function checkEquality(x, y) {
-    if(x == y || y == x){
-        return true;
-    }else{
-        return false;
-    }
+function countPoint(x, y) {
+    var pointer1 = 2;
+    var pointer2 = 3;
+    
+    return (x * pointer1) + (y * pointer2);
 }
-console.log(checkEquality(5, '5'));
+console.log(countPoint(2, 3));
+
